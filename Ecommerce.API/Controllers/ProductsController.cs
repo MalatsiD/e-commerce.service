181dd86b7cc042f865856dea1cdadb9a784f7852
Modise <modisemalatsi@gmail.com>
2025-03-16 17:38:23 +0200

    Changed to Repository

---
+++ Ecommerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,99 @@
+﻿using Ecommerce.Core.Entities;
+using Ecommerce.Core.Interfaces;
+using Ecommerce.Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts()
+        {
+            var result = await _productRepository.GetProductsAsync();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _productRepository.GetProductByIdAsync(id);
+
+            if (product == null)
+                return NotFound();
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> AddProduct(Product product)
+        {
+            _productRepository.AddProduct(product);
+            var isCreated = await _productRepository.SaveChangesAsync();
+
+            if (!isCreated)
+            {
+                return BadRequest("Problem creating product");
+            }
+
+            return CreatedAtAction("GetProduct", new {id = product.Id}, product);  
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> UpdateProduct(int id, Product product)
+        {
+            if (product.Id != id || !ProductExists(id))
+            {
+                return BadRequest("Cannot update this product");
+            }
+
+            _productRepository.UpdateProduct(product);
+            var isUpdated = await _productRepository.SaveChangesAsync();
+
+            if (!isUpdated)
+            {
+                return BadRequest("Problem updating the product");
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            var product = await _productRepository.GetProductByIdAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _productRepository.DeleteProduct(product);
+            var isDeleted = await _productRepository.SaveChangesAsync();
+
+            if(!isDeleted)
+            {
+                return BadRequest("Ploblem deleting a product");
+            }
+
+            return NoContent();
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _productRepository.ProductExists(id);
+        }
+    }
+}
---
+++ Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,56 @@
+﻿using Ecommerce.Core.Entities;
+using Ecommerce.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Infrastructure.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly StoreContext _storeContext;
+
+        public ProductRepository(StoreContext storeContext)
+        {
+            _storeContext = storeContext;
+        }
+
+        public void AddProduct(Product product)
+        {
+            _storeContext.Products.Add(product);
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            _storeContext.Products.Remove(product);
+        }
+
+        public async Task<Product?> GetProductByIdAsync(int id)
+        {
+            return await _storeContext.Products.FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            return await _storeContext.Products.ToListAsync();
+        }
+
+        public bool ProductExists(int id)
+        {
+            return _storeContext.Products.Any(x => x.Id == id);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await _storeContext.SaveChangesAsync() > 0;
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _storeContext.Entry(product).State = EntityState.Modified;
+        }
+    }
+}
