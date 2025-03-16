f025bd330e80f25922352a7a4bdd46d8ec419358
Modise <modisemalatsi@gmail.com>
2025-03-16 17:57:26 +0200

    Seeding data

---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,30 @@
+﻿using Ecommerce.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext storeContext)
+        {
+            if (!storeContext.Products.Any())
+            {
+                var productsData = await File.ReadAllTextAsync("../Infrastructure/Data/SeedData/products.json");
+
+                var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+
+                if (products == null)
+                    return;
+
+                await storeContext.Products.AddRangeAsync(products);
+
+                await storeContext.SaveChangesAsync();
+            }
+        }
+    }
+}
